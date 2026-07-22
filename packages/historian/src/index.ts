1f642749a4ea9775a3ea586fda67ffc3ac0559a0
Michael Lindenau <michael@nscale.io>
2026-07-22 14:57:52 -0400

    use historian for live view

--- packages/historian/src/index.ts
+++ packages/historian/src/index.ts
@@ -2,3 +2,4 @@ export * from "./types.js";
 export * from "./cursor.js";
 export * from "./registry.js";
 export { stationStateSeries, type StationStateScope, type StationStateRow } from "./station-state.js";
+export { metricBucketSeries, type MetricBucketScope, type MetricBucketRow } from "./metric-bucket.js";
